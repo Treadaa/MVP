1d06f061ff2f65b3c3759687705ca2db9930c553
Treadaa <87612934+Treadaa@users.noreply.github.com>
2021-10-07 01:05:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:final2/amplifyconfiguration.dart';
+import 'package:final2/frontpage.dart';
 import 'package:final2/homepage.dart';
 import 'package:final2/login_page.dart';
 import 'package:final2/providers.dart';
@@ -10,7 +12,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp((MyApp()));
 }
 
 class MyApp extends StatefulHookWidget {
@@ -19,76 +21,22 @@ class MyApp extends StatefulHookWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var userLoggedIn;
-  bool _amplifyConfigured = false;
-  bool checkAuthStatus = false;
-  late AmplifyAuthCognito auth;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _configureAmplify();
-  }
-
-  void _configureAmplify() async {
-    if (!mounted) return;
-    auth = AmplifyAuthCognito();
-    await Amplify.addPlugin(auth);
-    try {
-      Amplify.configure(amplifyconfig);
-    } on AmplifyAlreadyConfiguredException {
-      print("Already configured");
-    }
-
-    try {
-      getUserStatus();
-      setState(() {
-        _amplifyConfigured = true;
-      });
-    } catch (e) {
-      print(e);
-    }
   }
 
   @override
   Widget build(BuildContext context) {
-    userLoggedIn = useProvider(userLoggedInProvider);
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('TREADAA'),
-            centerTitle: true,
-          ),
-          body: _amplifyConfigured
-              ? checkAuthStatus
-                  ? userLoggedIn.getUserCurrentState()
-                      ? HomePage()
-                      : LoginPage()
-                  : Text('Loading')
-              : Text('Loading'),
-          // ignore: empty_statements
+        home: AnimatedSplashScreen(
+          splash: Icons.home,
+          nextScreen: FrontPage(),
+          splashTransition: SplashTransition.sizeTransition,
+          backgroundColor: Colors.amber,
+          duration: 30000,
         ));
-  }
-
-  getUserStatus() {
-    handleAuth().then((val) {
-      if (val.isSignedIn) {
-        userLoggedIn.setUserCurrentState(val.isSignedIn);
-        setState(() {
-          checkAuthStatus = true;
-        });
-      } else {
-        setState(() {
-          checkAuthStatus = true;
-        });
-        return LoginPage();
-      }
-    });
-  }
-
-  handleAuth() async {
-    var authStatus = await auth.fetchAuthSession();
-    return authStatus;
   }
 }
